dbfe837e673b3686d4ccdc4f0f2a9a15175ae386
NELSON Pereira de Castro <nelsoncastro.eireli@poupex.com.br>
2021-01-15 16:46:56 -0300

    adds delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,17 +1,20 @@
 function Tempo(props) {
-  console.log('Passando pelo Front-end');
+  console.log('> Passando pelo Front-end');
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
   return (
     <div>
         <div>{dynamicDateString} (dinâmico)</div>
-        <div>{props.staticDateString} (estático)</div>
+        <div>{props.staticDateString} (estático - com delay)</div>
     </div>
   )
 }
 
-export function getStaticProps() {
-  console.log('Passando pelo getStaticProps');
+export async function getStaticProps() {
+  console.log('> Passando pelo getStaticProps');
+  console.log('> Adicionando delay de 5 segundos');
+
+  await delay(5000);
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
@@ -21,5 +24,7 @@ export function getStaticProps() {
     }
   }
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
